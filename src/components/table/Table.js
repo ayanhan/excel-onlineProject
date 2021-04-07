aaa27b4fb9d79478b0249a79ebc75c791109d4ef
ayanhan <kalkhozhaev.j@gmail.com>
2021-04-07 21:12:00 +0900

    Resize done!

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.res';
+import {haveToResize} from '@/components/table/table.function';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
+    constructor($root) {
+      super($root, {
+        listeners: ['mousedown'],
+      })
+    }
+
 
     toHTML() {
       return createTable()
     }
+
+    onMousedown(event) {
+      if (haveToResize(event)) {
+        resizeHandler(this.$root, event)
+      }
+    }
 }
+
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,22 +2,30 @@ const CODES = {
   A: 65,
   Z: 90,
 }
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function createCol(el) {
+function createCol(el, index) {
   return `
-    <div class="column">${el}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${el}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(index, content) {
+  // eslint-disable-next-line max-len
+  const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : 'Ayan'}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : 'Ayan'}
+        ${resizer}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -32,8 +40,8 @@ export function createTable(rowsCount = 12) {
       .map((el, index) => {
         return String.fromCharCode(CODES.A + index)
       })
-      .map((el) => {
-        return createCol(el)
+      .map((el, index) => {
+        return createCol(el, index)
       })
       .join('')
 
