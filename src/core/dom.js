80269317a6e2478f71e4c2307647c8d5172f8f9e
ayanhan <kalkhozhaev.j@gmail.com>
2021-04-09 08:50:04 +0900

    Base logic finish!

--- src/core/dom.js
+++ src/core/dom.js
@@ -21,8 +21,12 @@ class Dom {
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
-  of(eventType, callback) {
+  off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector))
   }
 
   append(node) {
@@ -57,6 +61,44 @@ class Dom {
     Object.keys(styles).forEach((key) => {
       this.$el.style[key] = styles[key]
     })
+  }
+
+
+  addClass(className) {
+    this.$el.classList.add(className)
+    return this
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
+    return this
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
   }
 }
 
