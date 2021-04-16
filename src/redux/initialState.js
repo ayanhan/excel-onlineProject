66319b12195cf060d7604f63e4865fee825c9b78
ayanhan <kalkhozhaev.j@gmail.com>
2021-04-16 18:39:50 +0900

    Routing created.

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
-import {storage} from '@core/utils';
 import {defaultStyles, defaultTitle} from '@/constants';
+import {clone} from '@core/utils';
 
 const defaultState = {
   title: defaultTitle,
@@ -9,6 +9,7 @@ const defaultState = {
   stylesState: {},
   currentText: '',
   currentStyles: defaultStyles,
+  openedDate: new Date().toJSON(),
 }
 
 const normalize = (state) => ({
@@ -17,5 +18,6 @@ const normalize = (state) => ({
   currentText: '',
 })
 
-export const initialState = storage('state') ?
-    normalize(storage('state')) : defaultState
+export function normalizeInitialState(state) {
+  return state ? normalize(state) : clone(defaultState)
+}
